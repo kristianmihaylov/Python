e7a1ca0c1aa9230b2ba12580d94ceb215be2f097
kristianmihaylov <88734433+kristianmihaylov@users.noreply.github.com>
2021-08-23 22:08:41 +0300

    Delete basics/01. If directory

--- basics/01. If/invalid_number.py
+++
@@ -1,4 +0,0 @@
-number = int(input())
-
-if not (100 <= number <= 200 or number == 0):
-    print("invalid")
--- basics/01. If/number_in_range.py
+++
@@ -1,8 +0,0 @@
-# различно от 0 и в интервала от -100 до 100
-
-number = int(input())
-
-if number >= -100 and number <= 100 and number != 0:
-    print("Yes")
-else:
-    print("No")
